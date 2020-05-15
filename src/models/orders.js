f3be4e273a9aa4258ee481d4a69f1915c94728b6
Simon <shalimi5@reseau-ges.fr>
2020-05-15 15:04:28 +0200

    fix(order): Manually remove _id from subSchema and export part of order model in multiple schema

--- src/models/orders.js
+++ src/models/orders.js
@@ -1,22 +1,22 @@
 import db from '../db';
 
-
-export const model = db.model('Order', {
-  menus: [{
-    original_id: { type: String },
-    original_products: [{
-      original_id: { type: String },
-      original_name: { type: String },
-      original_category: { type: String },
-      original_price: { type: Number }
-    }]
-  }],
-  standalone_products: [{
+const productSchema = db.Schema(
+  {
     original_id: { type: String },
     original_name: { type: String },
     original_category: { type: String },
     original_price: { type: Number }
-  }],
+  }, { _id: false })
+
+const menuSchema = db.Schema({
+  original_id: { type: String },
+  original_products: [productSchema]
+}, { _id: false })
+
+
+export const model = db.model('Order', {
+  menus: [menuSchema],
+  standalone_products: [productSchema],
   price: { type: Number },
   status: { type: String, default: 'pending' },
   customer: { type: db.Types.ObjectId, ref: 'User' },
