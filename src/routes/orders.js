3a1e84ea71532cb34b9f2d49145aae7872b32146
NenetteGit <antonin.texier@gmail.com>
2020-05-15 20:23:55 +0200

    refactor(app): Rename succes by success.

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -89,7 +89,7 @@ router.post('/orders', guard({ requested_status: constants.CUSTOMER }),
 
       if (filledStandAloneProducts.errors) {
         return res.status(400).send({
-          succes: false,
+          success: false,
           errors: [filledStandAloneProducts.error],
         })
       } else {
