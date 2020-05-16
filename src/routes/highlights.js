839fdbb9a3d0f9167febf7745b44f73b96c60d9b
NenetteGit <antonin.texier@gmail.com>
2020-05-16 14:53:16 +0200

    feat(routes): Add new route to activate / deactivate a highlights.

--- src/routes/highlights.js
+++ src/routes/highlights.js
@@ -1,7 +1,11 @@
 import {Router} from 'express';
 
+import db from '../db'
+import * as constants from '../constants'
+
 import * as products from '../models/products';
 import * as menus from '../models/menus';
+import guard from "../middlewares/guard";
 
 const router = Router();
 export default router;
@@ -17,3 +21,28 @@ router.get('/highlights', async (req, res) => {
     return res.status(500).json({ success: false, errors: [e.message]});
   }
 });
+
+router.put('highlights/:id',
+  guard({ auth: constants.AUTH, requested_status: constants.ADMIN }), async (req, res) => {
+    try {
+      const _id = req.params.id || '';
+      const { promoted } = req.body || {};
+
+      if (!db.Types.ObjectId.isValid(_id)) {
+        return res.status(400).json({ success: false, errors: ['Invalid parameters.'], });
+      }
+
+      if (menus.model.exists({ _id })) {
+        await menus.model.updateOne({ _id }, { promoted });
+      } else if (products.model.exists({ _id })) {
+        await products.model.updateOne({ _id }, { promoted });
+      } else {
+        return res.status(404).json({ success: false, errors: ['Resource does not exist.'] });
+      }
+
+      res.json({ success : true });
+
+    } catch (e) {
+      return res.status(500).json({ success: false, errors: [e.message]});
+    }
+  });
