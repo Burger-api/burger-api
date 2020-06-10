97c627e6d34d26f41871025b7ffc25617b60636f
Simon <shalimi5@reseau-ges.fr>
2020-06-10 11:37:25 +0200

    feat(test): Add test libs

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,7 @@ export default router;
 
 router.post('/user/create', async (req, res) => {
   const { first_name, last_name, birthdate } = req.body || {}
-  
+
   const result = await users.model.create({
     first_name,
     last_name,
@@ -19,10 +19,32 @@ router.post('/user/create', async (req, res) => {
 
 });
 
-router.post('/user/create_todolist', async (req, res) => {
-  console.log(req.body)
+router.post('/user/:id/create_todolist', async (req, res) => {
+  const user = await users.model.findById(req.params.id)
+  const todolist_name = req.body.todolist_name
+
+  user.todolist = {
+    name: todolist_name,
+    todos: [],
+  }
+
+  user.save()
+
+  res.send({
+    status: "success",
+    result: user.todolist,
+  })
 })
 
-router.post('/user/add_action', async (req, res) => {
-  console.log(req.body)
+router.post('/user/:id/add_action', async (req, res) => {
+  const user = await users.model.findById(req.params.id)
+  const todolist_action = req.body.todolist_action
+
+  user.todolist.todos.push(todolist_action)
+  user.save()
+
+  res.send({
+    status: "success",
+    result: user
+  })
 })
---
+++ test/user.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert')
+
+describe("dog", () => {
+  it("should dig a hole", () => {
+    assert.equal(1, 1)
+  })
+})
